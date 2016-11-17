b1f20461806869cda69914d19fd8fac8c1f3c546
Yen Tran <yen.tran@autodesk.com>
2016-11-16 16:17:29 -0800

    deletes todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,12 @@
 $('li').click(function() {
 	$(this).toggleClass('completed');
 });
+
+//Click on X to delete Todo
+$('span').click(function(event){
+	//'this' is refering to the span
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove(); //'this' is referring to the 'li', not the span!
+	})
+	event.stopPropagation();
+});
