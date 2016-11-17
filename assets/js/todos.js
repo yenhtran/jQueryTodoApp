3e7b2edfa76bb8f1ec7cb9db71680ac72dd5f2af
Yen Tran <yen.tran@autodesk.com>
2016-11-16 16:01:24 -0800

    refactors the strike out functionality to use toggleClass

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,15 +1,4 @@
 // Check Off Specific Todos By Clicking
 $('li').click(function() {
-    //if li is grey
-    if ($(this).css('color') === 'rgb(128, 128, 128)') {
-        $(this).css({
-            color: 'black',
-            'text-decoration': 'none'
-        });
-    } else {
-        $(this).css({
-            color: 'gray',
-            'text-decoration': 'line-through'
-        });
-    }
+	$(this).toggleClass('completed');
 });
