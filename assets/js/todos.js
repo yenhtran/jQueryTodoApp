828f3c979160e45cb97bd50d57d398b1b90baf56
Yen Tran <yen.tran@autodesk.com>
2016-11-17 16:31:42 -0800

    creating todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,21 @@
 // Check Off Specific Todos By Clicking
-$('li').click(function() {
+$('ul').on('click','li',function() {
 	$(this).toggleClass('completed');
 });
 
 //Click on X to delete Todo
-$('span').click(function(event){
+$('ul').on('click', 'span',function(event){
 	//'this' is refering to the span
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove(); //'this' is referring to the 'li', not the span!
 	})
 	event.stopPropagation();
 });
+
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13){
+		var todoText = $(this).val();
+		$(this).val('');
+		$('ul').append('<li><span>X</span> ' + todoText + '</li>')
+	}
+})
