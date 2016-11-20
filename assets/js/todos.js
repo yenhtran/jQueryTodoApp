df519798a56dda531a668ced90481f4228cb85a9
Yen Tran <yen.tran@autodesk.com>
2016-11-19 16:23:25 -0800

    toggling the form

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -18,4 +18,8 @@ $("input[type='text']").keypress(function(event){
 		$(this).val('');
 		$('ul').append('<li><span><i class="fa fa-trash" aria-hidden="true"></i></span> ' + todoText + '</li>')
 	}
+});
+
+$('.fa-plus').click(function(){
+	$("input[type='text']").fadeToggle();
 })
